fca9ce80aa468eef463788616c6212f709626c1c
Ana Garrido <anagarrido@dpsoft.es>
2023-06-14 14:50:48 +0200

    first commit

---
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gif-card-list',
+  templateUrl: './card-list.component.html',
+
+})
+export class CardListComponent {
+
+
+}
